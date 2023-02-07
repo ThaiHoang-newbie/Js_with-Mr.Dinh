9dc941649263c2736fcc63b7181cdb981df5f68e
Thai Hoang <110578242+ThaiHoang-newbie@users.noreply.github.com>
2023-02-07 13:17:59 +0700

    Add files via upload

---
+++ 5thDay-Array/Ex2.js
@@ -0,0 +1,15 @@
+function ok(){
+    var username = document.getElementById("name").value;
+    var userpassword = document.getElementById("password").value;
+
+    var user = [
+        {
+            id : 1,
+            username: 
+        }
+
+
+    ]
+
+
+}
---
+++ 5thDay-Array/OnClass.js
@@ -0,0 +1,11 @@
+function myFunction(){
+    var mang = [];
+    var a = document.getElementById("name").value;
+    mang.push(a);
+    document.getElementById("dp").value = mang;
+}
+
+
+
+
+mang.push(mang[i].price = a);
